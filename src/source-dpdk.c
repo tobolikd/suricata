5b39297fc6a061f91d9588ee75ad000306d724ae
Lukas Sismis <lukas.sismis@gmail.com>
2022-08-01 13:42:09 +0200

    dpdk: add debug xstats

--- src/source-dpdk.c
+++ src/source-dpdk.c
@@ -380,6 +380,7 @@ static void DPDKBypassHardDelete(
 
 cleanup:
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(d->msg_mp, (void **)&msg, 1, NULL);
     }
 }
@@ -448,6 +449,7 @@ static void DPDKBypassSoftDelete(
 
 cleanup:
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(d->msg_mp, (void **)&msg, 1, NULL);
     }
 }
@@ -574,6 +576,7 @@ cleanup:
         LiveDevAddBypassFail(p->livedev, 1, AF_INET6);
 
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(p->dpdk_v.message_mp, (void **)&msg, 1, NULL);
     }
     return 0;
