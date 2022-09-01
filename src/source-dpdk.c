0359310adeb9815e0eb77598c4081860659f1a4e
Lukas Sismis <lukas.sismis@gmail.com>
2022-09-01 08:34:00 +0200

    dpdk: add strict message checks for async message consistency

--- src/source-dpdk.c
+++ src/source-dpdk.c
@@ -376,6 +376,7 @@ static void DPDKBypassHardDelete(Flow *f, struct DPDKFlowBypassData *d, struct r
 
 cleanup:
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(d->msg_mp, (void **)&msg, 1, NULL);
     }
 }
@@ -449,6 +450,7 @@ static void DPDKBypassSoftDelete(
 
 cleanup:
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(d->msg_mp, (void **)&msg, 1, NULL);
     }
 }
@@ -575,6 +577,7 @@ cleanup:
         LiveDevAddBypassFail(p->livedev, 1, AF_INET6);
 
     if (msg != NULL) {
+        msg->use_cnt--;
         rte_mempool_generic_put(p->dpdk_v.message_mp, (void **)&msg, 1, NULL);
     }
     return 0;
