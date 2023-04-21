7ef260d374dcb09e3c770eea2197dfc334e0ee2a
Andrei Shchapaniak <shchapaniak.andrei@gmail.com>
2023-04-21 06:37:52 -0400

    dpdk: change level of metadata info

--- src/runmode-dpdk.c
+++ src/runmode-dpdk.c
@@ -1708,18 +1708,20 @@ int OffloadsAgreemnet(DPDKIfaceConfig *iconf, struct PFConfRingEntry *pf_re, int
             SCReturnInt(-ENOENT);
         }
 
-        SCLogNotice("METADATA FROM PREFILTER TO SURICATA ON THE INTERFACE %s:\n"
-                    "\tOffload ipv4 (bit %d) is %s\n\tOffload ipv6 (bit %d) is %s\n"
-                    "\tOffload tcp (bit %d) is %s\n\tOffload udp (bit %d) is %s",
-                iconf->iface,
-                IPV4_ID, pf_re->oflds_final_IDS & IPV4_OFFLOAD(1) ? "enabled" : "disabled",
-                IPV6_ID, pf_re->oflds_final_IDS & IPV6_OFFLOAD(1) ? "enabled" : "disabled",
-                TCP_ID, pf_re->oflds_final_IDS & TCP_OFFLOAD(1) ? "enabled" : "disabled",
-                UDP_ID, pf_re->oflds_final_IDS & UDP_OFFLOAD(1) ? "enabled" : "disabled");
-        SCLogNotice("METADATA FROM SURICATA TO PREFILTER ON THE INTERFACE %s:\n"
-                    "\tOffload matchedRules (bit %d) is %s",
-                iconf->iface,
-                MATCH_RULES, pf_re->oflds_final_IPS & MATCH_RULES_OFFLOAD(1) ? "enabled" : "disabled");
+        if (i == 0) {
+            SCLogInfo("METADATA FROM PREFILTER TO SURICATA ON THE INTERFACE %s:\n"
+                      "\tOffload ipv4 (bit %d) is %s\n\tOffload ipv6 (bit %d) is %s\n"
+                      "\tOffload tcp (bit %d) is %s\n\tOffload udp (bit %d) is %s",
+                    iconf->iface,
+                    IPV4_ID, pf_re->oflds_final_IDS & IPV4_OFFLOAD(1) ? "enabled" : "disabled",
+                    IPV6_ID, pf_re->oflds_final_IDS & IPV6_OFFLOAD(1) ? "enabled" : "disabled",
+                    TCP_ID, pf_re->oflds_final_IDS & TCP_OFFLOAD(1) ? "enabled" : "disabled",
+                    UDP_ID, pf_re->oflds_final_IDS & UDP_OFFLOAD(1) ? "enabled" : "disabled");
+            SCLogInfo("METADATA FROM SURICATA TO PREFILTER ON THE INTERFACE %s:\n"
+                      "\tOffload matchedRules (bit %d) is %s",
+                    iconf->iface,
+                    MATCH_RULES, pf_re->oflds_final_IPS & MATCH_RULES_OFFLOAD(1) ? "enabled" : "disabled");
+        }
 
         SetIdxOfFinalOfflds(pf_re->oflds_final_IDS,
                 &iconf->cnt_offlds_suri_requested[i],
